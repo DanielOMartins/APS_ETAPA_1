c389c1a624b200a0a35a17bc145fa1348ba1e374
DanielOMartins <78224425+DanielOMartins@users.noreply.github.com>
2021-04-27 19:40:33 -0300

    Create AppLabirinto.java

---
+++ AppLabirinto.java
@@ -0,0 +1,16 @@
+public class AppLabirinto {
+    public static void main(String[] args) {
+        Labirinto labirinto = new Labirinto();
+        labirinto.iniciarLab();
+        labirinto.imprimirLab();
+        System.out.println("\n procurando solucao \n");
+        int linhainicio = labirinto.linhaInicio();
+        int colunainicio = labirinto.colunainicio();
+        boolean achou = labirinto.procurarCaminho(linhainicio, colunainicio);
+        if(achou){
+            System.out.println("achou caminho");
+        } else {
+            System.out.println("não tem caminho");
+        }
+    }
+}
